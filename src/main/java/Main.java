f1b4f09eb7f14317c1245485c249048e811e2dd5
camelion <mikroandi211@gmail.com>
2022-07-01 16:54:04 +0800

    Menambahkan report dari JasperSoft Studio

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ import frame.KecamatanViewFrame;
 public class Main {
     public static void main(String[] args) {
 //        Koneksi.getConnection();
-//        KabupatenViewFrame viewFrame = new KabupatenViewFrame();
-        KecamatanViewFrame viewFrame = new KecamatanViewFrame();
+        KabupatenViewFrame viewFrame = new KabupatenViewFrame();
+//        KecamatanViewFrame viewFrame = new KecamatanViewFrame();
         viewFrame.setVisible(true);
     }
 
